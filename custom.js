8872b3a95954d42b975c5821c2c10c829d8d9a1b
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-02 17:08:16 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+        var vds = document.getElementById('vds');
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/epfxk2@main/%EB%8D%B8%ED%83%802.mp4";
+        document.getElementById('vd').load();
+    });
